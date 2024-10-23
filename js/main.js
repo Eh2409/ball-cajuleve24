d2e83ec2b19e677df3a5fc48d1055621fb32b652
eliad <eliad.haim@gmail.com>
2024-10-24 00:40:02 +0300

    Add ball 3 functionality

--- js/main.js
+++ js/main.js
@@ -26,3 +26,27 @@ function changeBallSize(elBall,maxDiameter) {
 function changeBallColor(elBall) {
     elBall.style.backgroundColor = getRandomColor()
 }
+
+
+function onSwapBallsSizeAndColor() {
+    const elBall1 = document.querySelector('.ball1')
+    var ball1Size = +elBall1.innerText
+    var ball1Color = getComputedStyle(elBall1).backgroundColor
+ 
+    const elBall2 = document.querySelector('.ball2')
+    var ball2Size = +elBall2.innerText
+    var ball2Color = getComputedStyle(elBall2).backgroundColor
+
+    updateBALL(elBall1, ball2Size ,ball2Color)
+    updateBALL(elBall2, ball1Size ,ball1Color)
+    
+}
+
+function updateBALL(elBall, size ,color) {
+    elBall.style.width = size + 'px'
+    elBall.style.height = size + 'px'
+    elBall.innerText = size
+
+    elBall.style.backgroundColor = color
+}
+
