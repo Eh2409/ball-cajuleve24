2149228ad7b86a7a4f183aa4cacbd0d44df6c2d2
eliad <eliad.haim@gmail.com>
2024-10-23 21:00:56 +0300

    Add ball grow click

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 "use strict"
 
+var gBallSize = 100
+
 function onBallClick(elBall) {
-    console.log('shalom');
-    
+    gBallSize += 50
+    elBall.style.width = gBallSize + 'px'
+    elBall.style.height = gBallSize + 'px'
+
+    elBall.innerText =  gBallSize
 }
