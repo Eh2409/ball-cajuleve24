86f75f5641e2ff70f142cbd3b6e7883a00eedd5c
eliad <eliad.haim@gmail.com>
2024-10-24 00:58:04 +0300

    Add ball 4

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ function onBallClick(elBall, maxDiameter) {
     changeBallColor(elBall)
 }
 
-function changeBallSize(elBall,maxDiameter) {
+function changeBallSize(elBall, maxDiameter) {
     var currBallSize = +elBall.innerText
 
     if (currBallSize >= maxDiameter) {
@@ -16,7 +16,7 @@ function changeBallSize(elBall,maxDiameter) {
         var randomDiameterNum = getRandomInt(20, 61)
         currBallSize += randomDiameterNum
     }
-    
+
     elBall.style.width = currBallSize + 'px'
     elBall.style.height = currBallSize + 'px'
 
@@ -32,17 +32,17 @@ function onSwapBallsSizeAndColor() {
     const elBall1 = document.querySelector('.ball1')
     var ball1Size = +elBall1.innerText
     var ball1Color = getComputedStyle(elBall1).backgroundColor
- 
+
     const elBall2 = document.querySelector('.ball2')
     var ball2Size = +elBall2.innerText
     var ball2Color = getComputedStyle(elBall2).backgroundColor
 
-    updateBALL(elBall1, ball2Size ,ball2Color)
-    updateBALL(elBall2, ball1Size ,ball1Color)
-    
+    updateBALL(elBall1, ball2Size, ball2Color)
+    updateBALL(elBall2, ball1Size, ball1Color)
+
 }
 
-function updateBALL(elBall, size ,color) {
+function updateBALL(elBall, size, color) {
     elBall.style.width = size + 'px'
     elBall.style.height = size + 'px'
     elBall.innerText = size
@@ -50,3 +50,25 @@ function updateBALL(elBall, size ,color) {
     elBall.style.backgroundColor = color
 }
 
+function onReduceBallsSize() {
+    const elBall1 = document.querySelector('.ball1')
+    var ball1Size = +elBall1.innerText
+
+    const elBall2 = document.querySelector('.ball2')
+    var ball2Size = +elBall2.innerText
+
+    sizeReduce(elBall1, ball1Size)
+    sizeReduce(elBall2, ball2Size)
+}
+
+function sizeReduce(ball, ballSize) {
+
+    var randomDiameterNum = getRandomInt(20, 61)
+        ballSize-= randomDiameterNum
+
+        if (ballSize < 100) ballSize = 100
+    
+        ball.style.width = ballSize + 'px'
+        ball.style.height = ballSize + 'px'
+        ball.innerText = ballSize
+}
